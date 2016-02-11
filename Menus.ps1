fbdf0104c43f8fd184f0438bf4d6a7b1200cb8d2
TylerBoire <tylerboire@gmail.com>
2016-02-11 16:44:24 -0500

    Adding the first two files.

---
+++ Menus.ps1
@@ -0,0 +1,90 @@
+﻿#Tyler Boire
+#Intro to menus in powershell
+#2/4/2016
+#WTFPL
+
+#main menu that presents initial choices to the users and allows the script to be terminated. 
+function menu () {
+ 
+Write-Host "Welcome to NET300-01 Powershell for Admins menus"
+Write-Host "Please select an option from the menu."
+Write-Host "1.Security"
+Write-Host "2.Administration"
+Write-Host "3.exit"
+
+$choice = Read-Host "Selection: "
+
+#runs the command chosen by the user or exits the script. If the users picks and inncorrect choice, the script loops. 
+switch($choice)
+    {
+        1 {secMenu;break}
+        2 {adminMenu;break}
+        3 {exit;break}
+        default {Write-Host "That is not an option, please try again";menu}
+    }
+
+}
+
+
+#secondary menu for security options. 
+function secMenu() {
+    Write-Host "Security Menu: "
+    Write-Host "1. Get 100 event logs"
+    Write-Host "2. Get applied HotFixes"
+    Write-Host "3. Get running services"
+    Write-Host "4. Return to main menu"
+
+   $choice = Read-Host "Selection: "
+    
+#runs security commands based on user choice then returns to main menu. 
+switch($choice)
+    {
+        1{Get-EventLog -Newest 100;break}
+        2{Get-HotFix;break}
+        3{Get-Service|where{$_.Status -match "running" };break}
+        4{menu;break}
+        default {write-host "That is not an option, please try again";secMenu}
+    }
+
+#goes back to main menu after choice. 
+secMenu
+}
+
+#secondary menu for administration commands. 
+function adminMenu() {
+    Write-Host "Admin Menu: "
+    Write-Host "1. Get running process as CSV"
+    Write-Host "2. Get running process as HTML"
+    Write-Host "3. Start administrator shell"
+    Write-Host "4. Return to the main menu"
+
+    $choice = Read-Host "Selection: "
+
+	
+#runs commands based on the choice of the user. 
+switch($choice)
+    {
+		#asks where to save the CSV in absolute path notation then saves to the location. 
+        1{  $path = read-host "Where would you like to save the CSV?"; 
+            Get-Process| ConvertTo-Csv > $path;
+            break}
+
+		#asks where to save the HTML in absolute path notation then saves to the location. 
+        2{ $path = read-host "Where would you like to save the HTML";
+            Get-Process | ConvertTo-Html > $path;
+            break}
+
+        3{Start-Process powershell -Verb runas;break}
+
+        4{menu;break}
+
+        default {write-host "That is not an option, please try again";adminMenu}
+    }
+
+#loops back to main menu after the choice was ran. 
+adminMenu
+}
+
+
+#calls the main menu function.
+menu
---
+++ replace.ps1
@@ -0,0 +1,74 @@
+﻿#Tyler Boire
+#replacement script
+#2/5/2016
+#WTFPL
+
+function getInfo(){
+#this prompts the user for information about what they would like to do.
+
+$file = Read-Host -Prompt "What file would you like to search?" 
+
+$term1 = Read-Host -Prompt "What term would you like to replace?"
+
+$term2 = Read-Host -Prompt "What would you like to replace that term with?"
+
+validate
+}
+
+function validate() {
+#this allows the user to confirm that they would like to continue with the information they provided. 
+
+write ("File to open: " + $file )
+write ("Term to search: " + $term1)
+write ("Term to replace with: " + $term2)
+
+    $correct = Read-Host -Prompt "is this information correct?[Yes/No]"
+
+    if($correct -eq "Yes" -or $correct -eq "yes"){
+    replace
+    }
+
+    else{
+    getInfo
+    }
+}
+
+function replace(){
+#this prints the replacement to the screen so the user can confirm the replacement worked correctly.
+
+Write-Host "please Verify the information is as expected"
+
+(cat $file).replace($term1,$term2) |out-host
+
+    $correct2 = Read-Host -Prompt "is this information correct?[Yes/No]"
+
+    #confirms the replacement went as planned or re-prompts for the information needed
+    if($correct2 -eq "Yes" -Or $correct2 -eq "yes"){
+    
+            #Asks the user if they would like to save to the file they specified earlier. 
+            $wantTosave = Read-Host -Prompt "Do you want to save this info?[Yes/No]"
+            
+
+            #Shames the user if they dont want to save or saves the file. 
+            if ($wantTosave -eq "Yes" -Or $wantTosave -eq "yes"){
+
+                #prompts user for save location then saves the file and alerts them it saved. 
+                $save = Read-Host -Prompt "Where would you like to save the file?: ";
+                (cat $file).replace($term1,$term2) |Set-Content $save; 
+                Write-Host "File Saved Successfuly"
+                }
+
+            else{
+                #user beratement. 
+                Write-Host "LAME!" -BackgroundColor Black -ForegroundColor Red
+            }
+
+    }
+
+    else{
+    getInfo
+    }
+
+}
+
+getInfo
